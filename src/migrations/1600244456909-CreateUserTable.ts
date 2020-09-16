c1b0233699be298b3892220821615cda1e93277a
anne castrillon <>
2020-09-16 01:48:21 -0700

    created users table

---
+++ src/migrations/1600244456909-CreateUserTable.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class CreateUserTable1600244456909 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+        `create table users(
+            id serial primary key,
+            email varchar(255) unique not null,
+            password varchar(255) unique not null,
+            description varchar(255) default null,
+            gender char(1),
+            profile_image_url text default null,
+            push_enabled boolean default null,
+            banner_image_url text default null
+        )`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`drop table users`)
+    }
+}
