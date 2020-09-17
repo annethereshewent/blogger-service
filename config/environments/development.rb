84d046001d839b380b206f065412e678a78b2ba1
anne castrillon <>
2020-09-17 10:21:00 -0700

    added support for JWT and set error response to api

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,9 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
 
+  config.debug_exception_response_format = :api
+
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
